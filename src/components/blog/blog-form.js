446327eea8e885d3b0263cc052cb5bf6ce6697c9
Tristan Mouritsen <tmouritsen57@gmail.com>
2021-11-04 16:43:27 -0600

    implemented rich text editor inside blog modal, updated blog detail to render HTML stylings, connected blog modal to portfolio API

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,6 +1,8 @@
 ﻿import React, { Component } from 'react';
 import axios from 'axios';
 
+import RichTextEditor from '../forms/rich-text-editor';
+
 export default class BlogForm extends Component {
 
     constructor(props) {
@@ -8,10 +10,18 @@ export default class BlogForm extends Component {
         this.state = {
             title: "",
             blog_status: "",
+            content: "",
         }
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this);
+    }
+
+    handleRichTextEditorChange(content) {
+        this.setState({
+            content
+        })
     }
 
     buildForm() {
@@ -19,6 +29,7 @@ export default class BlogForm extends Component {
 
         formData.append("portfolio_blog[title]", this.state.title);
         formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+        formData.append("portfolio_blog[content]", this.state.content);
 
         return formData;
     }
@@ -29,11 +40,12 @@ export default class BlogForm extends Component {
             this.buildForm(),
             {withCredentials: true}
             ).then(response => {
-                this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
                 this.setState({
                     title: "",
                     blog_status: "",
+                    content: "",
                 })
+                this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog);
             }).catch(error => {
                 console.log("handleSubmit new blog post error", error)
             });
@@ -71,8 +83,13 @@ export default class BlogForm extends Component {
                     </select>
                 </div>
 
+                <div className="one-column">
+                    <RichTextEditor handleRichTextEditorChange={this.handleRichTextEditorChange} />
+                </div>
+
                 <button className="btn">Save</button>
             </form>
         )
     }
 }
+// if you're ever setting state, and variable name for the key is going to be the same as it's value, you don't need to say "content: content", you can just say "content" and JavaScript will understand!
---
+++ src/components/forms/rich-text-editor.js
@@ -0,0 +1,38 @@
+﻿import React, { Component } from 'react';
+import { EditorState, convertToRaw } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import draftToHtml from 'draftjs-to-html';
+import htmlToDraft from 'html-to-draftjs';
+
+export default class RichTextEditor extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            editorState: EditorState.createEmpty()
+        }
+        this.onEditorStateChange = this.onEditorStateChange.bind(this);
+    }
+
+    onEditorStateChange(editorState) {
+        this.setState({
+            editorState
+        }, this.props.handleRichTextEditorChange(
+            draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
+            ));
+        //you can't trust this.setState to happen immediately- it's asynchronouse! Don't expect to use the new state if you call it immediately afterward, within the same function! However, you can pass in a second argument which will only fire after state is updated!
+    }
+
+    render() {
+        return (
+            <div>
+                <Editor
+                editorState={this.state.editorState}
+                wrapperClassName="demo-wrapper"
+                editorClassName="demo-editor"
+                onEditorStateChange={this.onEditorStateChange}
+                />
+            </div>
+        )
+    }
+}
